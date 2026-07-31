9a102e205d2ff17787fd72403cca73eeeeb6598c
songhwaplay <stimpack@naver.com>
2026-07-31 11:11:20 +0900

    fix(periodic-table): remove misleading fixed-exam label

--- tests/periodic-table-molecule-models.js
+++ tests/periodic-table-molecule-models.js
@@ -92,6 +92,7 @@ assert.doesNotMatch(appSource, /function renderMoleculeSVG\(/);
 assert.match(pageSource, /id="molecule3dCanvas"/);
 assert.doesNotMatch(pageSource, /시험 범위만 한눈에/);
 assert.doesNotMatch(pageSource, /class="exam-focus"/);
+assert.doesNotMatch(pageSource, /1~20번 고정 출제/);
 assert.doesNotMatch(pageSource, /id="modalDiscovery"/);
 assert.doesNotMatch(pageSource, /id="modalNameEn"/);
 assert.match(pageSource, /id="modalDesc"/);
