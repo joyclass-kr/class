1f79c037324ad17585201f603d56a6e7aa2cb008
songhwaplay <stimpack@naver.com>
2026-08-24 07:36:46 +0900

    Replace mascot favicon with Joyclass J mark

--- game-hub-server/server.js
+++ game-hub-server/server.js
@@ -242,6 +242,10 @@ app.get("/favicon-20260824-v5.ico", (_req, res) => {
   res.setHeader("Cache-Control", "public, max-age=31536000, immutable");
   res.sendFile(path.join(SITE_ROOT, "favicon-20260824-v5.ico"));
 });
+app.get("/favicon-20260824-v6.ico", (_req, res) => {
+  res.setHeader("Cache-Control", "public, max-age=31536000, immutable");
+  res.sendFile(path.join(SITE_ROOT, "favicon-20260824-v6.ico"));
+});
 app.get("/favicon.png", (_req, res) => {
   res.setHeader("Cache-Control", "no-cache, must-revalidate");
   res.sendFile(path.join(SITE_ROOT, "favicon.png"));
